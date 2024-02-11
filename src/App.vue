ad088d97acce77139864f91246f356d928d1eb01
Sce24 <christianochea@gmail.com>
2024-02-11 10:06:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import {ref} from 'vue'
+const message = ref('Hello')
+let res = ref('')
+let num = ref('')
+
+const grade =()=>
+{
+    if(num.value == null) {
+        res.value = 'Invalid Input'
+        return;
+    }
+
+    if(num.value > 60) {
+        res.value = "Pass"
+    }
+    else {
+        res.value = "Fail"
+    }
+}
+</script>
+
+<template>
+<div>
+    <p>{{ message }}</p>
+    <h1>Enter your Grade</h1>
+    <input type="number" v-model="num">
+    <input type="submit" value="Validate" @click="grade">
+    <br>
+    <h3 :class="{'pass' :res== 'Pass', 'fail' :  res =='Fail'}">{{ res || 'No input'}}</h3>
+</div>
+</template>
+
+<style>
+.pass {
+    color: green;
+}
+.fail {
+    color: red;
+}
+</style>
