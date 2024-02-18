5bddb3b70fd2d2854c97936bc378e849d61b1961
Sce24 <christianochea@gmail.com>
2024-02-18 11:02:34 +0800

    changed the code into a system that will identify if the grade is fail or pass

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,70 @@
 <script setup>
 import {ref} from 'vue'
-const message = ref('Hello')
-let res = ref('')
-let num = ref('')
 
-const grade =()=>
-{
-    if(num.value == null) {
-        res.value = 'Invalid Input'
-        return;
-    }
+const name = ref('')
+const ave = ref('')
+const students = ref([])
+const compute = ref('')
+let message = ('')
+const Check = ()=>{
 
-    if(num.value > 60) {
-        res.value = "Pass"
-    }
-    else {
-        res.value = "Fail"
-    }
+    if(ave.value < 60) {
+  students.value.push({fname:name.value, average:ave.value, compute:"Fail"})
 }
+ else {
+    students.value.push({name:name.value, average:ave.value, compute:"Pass"})
+ }
+  name.value = ''
+  ave.value = ''
+
+}
+
 </script>
 
 <template>
+<header>
+  <h1>Oral</h1>
+</header>
 <div>
-    <p>{{ message }}</p>
-    <h1>Enter your Grade</h1>
-    <input type="number" v-model="num">
-    <input type="submit" value="Validate" @click="grade">
-    <br>
-    <h3 :class="{'pass' :res== 'Pass', 'fail' :  res =='Fail'}">{{ res || 'No input'}}</h3>
+  <input type="text" v-model= "name" placeholder="Enter Student Name">
+  <input type="number" v-model = "ave" placeholder="Average">
+  <input type="submit" value="Enter" @click="Check">
+
+  <table >
+    <thead>
+      
+        <th>Name</th>
+        <th>Average</th>
+        <th>Pass or Fail</th>
+    </thead>
+    <tbody v-for ="(student,index) in students" :key="index" >
+      
+        <td>{{ student.fname }}</td>
+        <td>{{ student.average }}</td>
+        <td :class="{'pass' :student.compute =='Pass', 'fail' :student.compute=='Fail'}" >
+        {{ student.compute }}</td>
+    </tbody>
+  </table>
 </div>
 </template>
 
 <style>
-.pass {
-    color: green;
-}
+
 .fail {
-    color: red;
+ background-color: red;
 }
+.pass {
+    background-color: green;
+}
+body{
+    background-color: white;
+    color: black;
+}
+table {
+    border:1px solid black
+}
+th,td{
+    border:1px solid black
+}
+
 </style>
